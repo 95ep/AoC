e24103bda21bb9830ae71196aba4e7d08d9d1f1e
95ep <erik.ao.persson@gmail.com>
2020-12-01 22:04:53 +0100

    Day 1 commit

---
+++ day1.py
@@ -0,0 +1,13 @@
+import csv
+
+with open('inputs/day1.csv') as csvfile:
+    data_iter = csv.reader(csvfile, delimiter='\n')
+    data = [int(entry[0]) for entry in data_iter]
+
+for i1, d1 in enumerate(data):
+    for i2 in range(i1+1, len(data)):
+        d2 = data[i2]
+        for i3 in range(i2+1, len(data)):
+            d3 = data[i3]
+            if d1 + d2 + d3 == 2020:
+                print(d1*d2*d3)
