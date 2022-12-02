9b851eef70224b4db59bf1d698e68edac57bb782
95ep <erik.ao.persson@gmail.com>
2022-12-02 07:21:29 +0100

    Day 02 solution

---
+++ tests/y2022/test_day02.py
@@ -0,0 +1,28 @@
+import time
+from y2022.day02 import solution_1, solution_2
+
+
+def test_solution_1():
+    example_input_path = "tests/y2022/inputs/day02.txt"
+    example_results = 15
+    assert solution_1(example_input_path) == example_results
+    print("Tests 1 successful")
+
+
+def test_solution_2():
+    example_input_path = "tests/y2022/inputs/day02.txt"
+    example_result = 12
+    assert solution_2(example_input_path) == example_result
+    print("Tests 2 successful")
+
+
+if __name__ == "__main__":
+    tic = time.time()
+    test_solution_1()
+    toc = time.time()
+    print(f"****** Test 1 completed in {toc-tic} seconds! ********")
+
+    tic = time.time()
+    test_solution_2()
+    toc = time.time()
+    print(f"****** Test 2 completed in {toc-tic} seconds! ********")
---
+++ y2022/day02.py
@@ -0,0 +1,66 @@
+from utils.readers import reader_split_by_line
+
+MOVE_SCORE = {"X": 1, "Y": 2, "Z": 3}
+
+
+def determine_outcome(move_1, move_2):
+    if move_1 == move_2:
+        return 3
+    elif (move_1 + 1) % 3 == move_2:
+        return 6
+    else:
+        return 0
+
+
+def calc_score_for_round(opponent_move, our_move):
+    score = our_move + 1
+    score += determine_outcome(opponent_move, our_move)
+    return score
+
+
+def determine_move(opponent_move, outcome):
+    if outcome == 0:
+        our_move = (opponent_move - 1) % 3
+    elif outcome == 1:
+        our_move = opponent_move
+    else:
+        our_move = (opponent_move + 1) % 3
+
+    return our_move
+
+
+def calc_score_for_round_v2(opponent_move, outcome):
+    our_move = determine_move(opponent_move, outcome)
+    return calc_score_for_round(opponent_move, our_move)
+
+
+def convert_to_num(instruction):
+    val1 = ord(instruction[0]) - ord("A")
+    val2 = ord(instruction[2]) - ord("X")
+    return val1, val2
+
+
+def solution_1(input_path):
+    input = reader_split_by_line(input_path)
+    tot_score = 0
+    for instruction in input:
+        opponent_move, outcome = convert_to_num(instruction)
+        tot_score += calc_score_for_round(opponent_move, outcome)
+
+    return tot_score
+
+
+def solution_2(input_path):
+    input = reader_split_by_line(input_path)
+    tot_score = 0
+    for instruction in input:
+        opponent_move, outcome = convert_to_num(instruction)
+        tot_score += calc_score_for_round_v2(opponent_move, outcome)
+
+    return tot_score
+
+
+if __name__ == "__main__":
+    input_path = "y2022/inputs/day02.txt"
+    print(f"Answer to part 1 is: {solution_1(input_path)}")
+    print(f"Answer to part 2 is: {solution_2(input_path)}")
