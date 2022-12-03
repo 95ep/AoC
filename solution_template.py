cea6b5868d0852ddb1f8c2f425a3d52fc17e05d8
95ep <erik.ao.persson@gmail.com>
2022-12-03 16:30:19 +0100

    Add solution and test templates

---
+++ solution_template.py
@@ -0,0 +1,12 @@
+def solution_1(input_path):
+    pass
+
+
+def solution_2(input_path):
+    pass
+
+
+if __name__ == "__main__":
+    input_path = ""
+    print(f"Answer to part 1 is: {solution_1(input_path)}")
+    print(f"Answer to part 2 is: {solution_2(input_path)}")
