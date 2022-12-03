e0b31f80820a39b414b13eb102471b1e43689096
95ep <erik.ao.persson@gmail.com>
2022-12-03 17:03:10 +0100

    Update solution template

--- solution_template.py
+++ solution_template.py
@@ -7,6 +7,6 @@ def solution_2(input_path):
 
 
 if __name__ == "__main__":
-    input_path = ""
+    input_path = "y202x/inputs/dayXX.txt"
     print(f"Answer to part 1 is: {solution_1(input_path)}")
     print(f"Answer to part 2 is: {solution_2(input_path)}")
