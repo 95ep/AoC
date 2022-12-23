19522dc45a1c17ff1baa5e2b2c7a24f5fc24ab29
95ep <erik.ao.persson@gmail.com>
2022-12-23 16:34:33 +0100

    Add timestamp to solution template

--- solution_template.py
+++ solution_template.py
@@ -1,3 +1,6 @@
+import time
+
+
 def solution_1(input_path):
     pass
 
@@ -8,5 +11,11 @@ def solution_2(input_path):
 
 if __name__ == "__main__":
     input_path = "y202x/inputs/dayXX.txt"
+    tic = time.time()
     print(f"Answer to part 1 is: {solution_1(input_path)}")
+    toc = time.time()
+    print(f"****** Part 1 calculated in {toc-tic} seconds! ********")
+    tic = time.time()
     print(f"Answer to part 2 is: {solution_2(input_path)}")
+    toc = time.time()
+    print(f"****** Part 2 calculated in {toc-tic} seconds! ********")
